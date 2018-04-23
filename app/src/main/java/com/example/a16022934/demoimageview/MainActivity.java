32d79d64245fad58a49cf92cddaad9630b52115a
Javier <16022934@myrp.edu.sg>
2018-04-23 09:59:06 +0800

    Final Commit

--- app/src/main/java/com/example/a16022934/demoimageview/MainActivity.java
+++ app/src/main/java/com/example/a16022934/demoimageview/MainActivity.java
@@ -6,15 +6,24 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView iv;
+    ImageView iv2;
+    ImageView iv3;
+    ImageView iv4;
+    ImageView iv5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        iv = findViewById(R.id.imageView2);
+        iv2 = findViewById(R.id.imageView2);
+        iv3 = findViewById(R.id.imageView3);
+        iv4 = findViewById(R.id.imageView4);
+        iv5 = findViewById(R.id.imageView5);
 
-        iv.setImageResource(R.drawable.day2);
+        iv2.setImageResource(R.drawable.day2);
+        iv3.setImageResource(R.drawable.day3);
+        iv4.setImageResource(R.drawable.day4);
+        iv5.setImageResource(R.drawable.day5);
     }
 }
